d32f216a9866f42156b10ee51fb0c44dcd9ee07e
toosa <pht854@gmail.com>
2023-01-15 12:55:31 +0700

    setting.php updated

---
+++ setting.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Custom Dashboard local version details
+ *
+ * @package    local_tpdashboard
+ * @copyright  2023 Prihantoosa
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($hassiteconfig) {
+
+    $ADMIN->add('root', new admin_category('tpdashboard', get_string('pluginname',local_tpdashboard)));
+    
+    $ADMIN->add('tpdashboard', new admin_externalpage('userdata', get_string('userdata',local_tpdashboard),
+                                new moodle_url('/local/tpdashboard/userdata.php')));
+
+    $ADMIN->add('tpdashboard', new admin_externalpage('userdata', get_string('userdata',local_tpdashboard),
+                                new moodle_url('/local/tpdashboard/userdata.php')));
+}
