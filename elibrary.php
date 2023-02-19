fc006b7e81d499990ba3778a70c368542d9ca757
Prihantoosa <toosa@users.noreply.github.com>
2023-02-20 04:18:44 +0700

    Update elibrary.php
    setup awal elibrary iframe

--- elibrary.php
+++ elibrary.php
@@ -1,1 +1,28 @@
+<?php
+//...
 
+/**
+ * Custom Dashboard local version details
+ *
+ * @package    local_tpdashboard/elibrary
+ * @copyright  2023 Prihantoosa
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ require_once('../../config.php');
+
+ $context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/tpdashboard/elibrary.php'));
+$PAGE->set_pagelayout('dashboard');
+// $PAGE->set_title(get_string('pluginname','local_tpdashboard'));
+
+$PAGE->set_title('Elibrary');
+
+echo $OUTPUT->header();
+
+echo $OUTPUT->render_from_template('local_tpdashboard/elibrary',[
+  'title'=> 'Elibrary'
+]);
+
+echo $OUTPUT->footer();
