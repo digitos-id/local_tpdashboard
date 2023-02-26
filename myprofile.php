200029dec8469c010bb28af62c238021044fb353
Prihantoosa <toosa@users.noreply.github.com>
2023-02-26 10:46:23 +0700

    Update myprofile.php
    Add function getprofilepictureurl

--- myprofile.php
+++ myprofile.php
@@ -24,3 +24,28 @@ echo $OUTPUT->render_from_template('local_tpdashboard/myprofile_template',['teks
 // echo '<h1>Personal Dashboard</h1>';
 
 echo $OUTPUT->footer();
+
+
+ /**
+     * Retrieves the URL for the user's profile picture, if one is available.
+     * 
+     * @param object $user The Moodle user object for which we want a photo.
+     * @return string URL to the photo image file but with $1 for the size.
+     */
+function getprofilepictureurl($user) { 
+        if (isloggedin() && !isguestuser() && $user->picture > 0) {
+            $usercontext = context_user::instance($user->id, IGNORE_MISSING);
+            $url = moodle_url::make_pluginfile_url($usercontext->id, 'user', 'icon', null, '/', "f$1")
+                    . '?rev=' . $user->picture;
+        } else {
+            // If the user does not have a profile picture, use the default faceless picture.
+            global $PAGE, $CFG;
+            $renderer = $PAGE->get_renderer('core');
+            if ($CFG->branch >= 33) {
+                $url = $renderer->image_url('u/f$1');
+            } else {
+                $url = $renderer->pix_url('u/f$1'); // Deprecated as of Moodle 3.3.
+            }
+        }
+        return str_replace('/f%24', '/f$', $url);
+    }
