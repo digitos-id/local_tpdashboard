24b9b0ba96bed0ed7a34bb0b9e74861676e89681
Prihantoosa <pht854@gmail.com>
2023-02-27 05:30:13 +0700

    link dari menu ke Kalender, TentangTrec,Dasboard

--- elearningmenu.php
+++ elearningmenu.php
@@ -10,57 +10,47 @@
  * 
  */
 
-global $USER;
+ global $USER;
 
-require_once('../../config.php');
-require_once($CFG->dirroot.'/user/profile/lib.php');
-
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url(new moodle_url('/local/tpdashboard/elearningmenu.php'));
-$PAGE->set_pagelayout('dashboard');
-$PAGE->set_title(get_string('myprofiletitle','local_tpdashboard'));
-
-$userid = $USER->id;
-$user_object = core_user::get_user($userid);
-
-$firstname = $USER->firstname;
-$subholding = $USER->profile[subholding];
-$subcompany = $USER->profile[subcompany];
-$email = $USER->email;
-
-// echo $USER->profile[subholding];
-//    var_dump($USER);
-//    die();
-
-
-// $conditions = array('size' => '100', 'link' => false, 'class' => '');
-//$person_profile_pic = $OUTPUT->user_picture($user_object, $conditions);
-# echo $person_profile_pic;
- # echo '<img src="'.$getprofilepictureurl($userid).'">';
-# $urlgbr = getprofilepictureurl($userid);
-#var_dump($urlgbr);
-#die();
-#
-$description = "<b>description</b>";
- $data = [
-        'title1' => "",
-        'title2' => "",
-        'firstname' => $firstname,
-        'email' => $email,
-        'subholding' => $subholding,
-        'subcompany' => $subcompany,
-        'profileimgurl' => getprofilepictureurl($user_object),
-        'description' => format_text($description, FORMAT_HTML),
-        'url_tentangtrec' => "https://enlight-dev.digitos.id/local/tpdashboard/tentangtrec.php";
- ];
-// var_dump($data);
- // die();
- //
+ require_once('../../config.php');
+ require_once($CFG->dirroot.'/user/profile/lib.php');
  
-echo $OUTPUT->header();
-echo $OUTPUT->render_from_template('local_tpdashboard/elearningmenu_template',$data);
-// echo '<h1>Personal Dashboard</h1>';
-
-echo $OUTPUT->footer();
+ $context = context_system::instance();
+ $PAGE->set_context($context);
+ $PAGE->set_url(new moodle_url('/local/tpdashboard/elearningmenu.php'));
+ $PAGE->set_pagelayout('dashboard');
+ $PAGE->set_title(get_string('myprofiletitle','local_tpdashboard'));
+ 
+ $userid = $USER->id;
+ $user_object = core_user::get_user($userid);
+ 
+ $firstname = $USER->firstname;
+ $subholding = $USER->profile[subholding];
+ $subcompany = $USER->profile[subcompany];
+ $email = $USER->email;
+ 
+ // echo $USER->profile[subholding];
+ //    var_dump($USER);
+ //    die();
+ 
+ $description = "<b>description</b>";
+  $data = [
+         'title1' => "",
+         'title2' => "",
+         'firstname' => $firstname,
+         'email' => $email,
+         'subholding' => $subholding,
+         'subcompany' => $subcompany,
+         'description' => format_text($description, FORMAT_HTML),
+         'url_tentangtrec' => "https://enlight-dev.digitos.id/local/tpdashboard/tentangtrec.php"
+  ];
+ // var_dump($data);
+  // die();
+  //
+ 
+ echo $OUTPUT->header();
+ echo $OUTPUT->render_from_template('local_tpdashboard/elearningmenu_template',$data);
+ // echo '<h1>Personal Dashboard</h1>';
+ 
+ echo $OUTPUT->footer();
 
