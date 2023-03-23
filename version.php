b8d24ece6fa94a8a715d8c4f5c356d99436dacc8
Prihantoosa <toosa@users.noreply.github.com>
2023-03-23 16:14:23 +0700

    Update version.php
    Version 03

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023032202;
+$plugin->version   = 2023032203;
 $plugin->requires  = 2021042100;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'Version for Moodle 3.11 onwards';
